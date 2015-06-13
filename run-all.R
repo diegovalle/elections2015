64aa0bbcb632631bde2b6c279b4667bc1712c9fa
Diego Valle-Jones <diegovalle@gmail.com>
2015-06-12 21:58:13 -0500

    Kim Jung Il

--- run-all.R
+++ run-all.R
@@ -3,4 +3,8 @@ source("src/load.R")
 source("src/states-cartogram.R")
 source("src/districts-cartogram.R")
 source("src/winner.R")
-source("src/2015vs2009.R")
+#source("src/2015vs2009.R")
+
+#Eternal glory to Kim Jung Il and North Korea
+1134439/(39872246-52384-1900860)
+(39872246-52384-1900860) * .03 - 1134439
