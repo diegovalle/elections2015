6101e75c5f66a8cc62b606c30bd9cef8cf02785c
Diego Valle-Jones <diegovalle@gmail.com>
2015-06-14 14:08:49 -0500

    generate merge fields with party data for merging with the topojson map

--- run-all.R
+++ run-all.R
@@ -3,20 +3,10 @@ source("src/load.R")
 source("src/states-cartogram.R")
 source("src/districts-cartogram.R")
 source("src/winner.R")
+source("src/topo-for-merge.R")
 #source("src/2015vs2009.R")
 
 #Eternal glory to Kim Jung Il and North Korea
 1134439/(39872246-52384-1900860)
 (39872246-52384-1900860) * .03 - 1134439
 
-
-#topojson -o final.json -e topo.csv --id-property=+CLAVEGEO -p PRI=+PRI_T,+PAN,PRD=+PRD_T,+MORENA,PVEM=+PVEM_T,+MOVIMIENTO_CIUDADANO,+NUEVA_ALIANZA,+PS,PRIPVEM=+C_PRI_PVEM_T,PRDPT=+C_PRD_PT,NOMDISTRITO -- distritos.json
-head(districts)
-names <- read_csv("data/distrito-names.csv")
-write.csv(districts[,c("ESTADO", "DISTRITO", "PARTIDO", "per", "id")] %>%
-  mutate(per = round(per, 3)) %>%
-  spread(PARTIDO, per) %>%
-    rename(CLAVEGEO = id) %>%
-  left_join(names), "map/topo.csv", row.names = FALSE)
-
-max(districts$per)
---
+++ src/topo-for-merge.R
@@ -0,0 +1,19 @@
+
+#topojson -o final.json -e topo.csv --id-property=+CLAVEGEO -p PRI=+PRI_T,+PAN,PRD=+PRD_T,+MORENA,PVEM=+PVEM_T,+MOVIMIENTO_CIUDADANO,+NUEVA_ALIANZA,+PS,PRIPVEM=+C_PRI_PVEM_T,PRDPT=+C_PRD_PT,NOMDISTRITO -- distritos.json
+head(districts)
+names <- read_csv("data/distrito-names.csv")
+topo <- districts[,c("ESTADO", "DISTRITO", "PARTIDO", "per", "id")] %>%
+  mutate(per = round(per, 3)) %>%
+  spread(PARTIDO, per) %>%
+  left_join(winner) %>%
+  rename(CLAVEGEO = id) %>%
+  left_join(names)
+topo$color <- plyr::revalue(topo$winner, c("PRI" = "#ec242a", "PAN" = "#02569b", "PRD" = "#ffff00",
+                                           "MORENA" = "#610200", "PVEM" = "#2ca25f", "MOVIMIENTO_CIUDADANO" = "#f58e1e",
+                                           "NUEVA_ALIANZA" = "#37b4b7",
+                                           "C_PRI_PVEM_T" = "#bd0026",
+                                           "C_PRD_PT_T" = "#d5b60a")) 
+write.csv(topo, "map/topo.csv", row.names = FALSE)
+
+which.max(topo$C_PRI_PVEM_T)
+max(districts$per)
